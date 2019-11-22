982763b1cbd288b04ec69d8e66661d91de12e9d3
osoman2 <osmancampeon69@hotmail.com>
2019-11-21 21:38:59 -0500

    print()gracias dario

--- main.cpp
+++ main.cpp
@@ -2,16 +2,11 @@
 
 int main(){
     Trie a;
-    a.insert("hola");
-    a.insert("holaa");
-    a.insert("holaaa");
-    a.insert("Os");
-    a.insert("Osm");
-    cout <<a.search_by_complete_word("hola")<<endl;
-    a.delete_("hola");
-    cout <<a.search_by_complete_word("Os")<<endl;
-    cout <<a.search_by_complete_word("hola")<<endl;
-    cout <<a.search_by_complete_word("holaaa")<<endl;
+    
+    
+    a.desde_ar_n("o.txt");
     a.print();
+     
+
     return 0;
 }
